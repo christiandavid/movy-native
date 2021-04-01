ed7715b3776b2ac9b51c1b050f5d58229eaac67e
Christian David Ibarguen <christianibarguen@gmail.com>
2021-04-01 14:34:24 -0500

    Update index components

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { Button } from '@/components/Button';
 export { ErrorView } from '@/components/ErrorView';
 export { TabBarIcon } from '@/components/TabBarIcon';
 export { TextField } from '@/components/TextField';
+export { MoviePoster } from '@/components/MoviePoster';
+export { HorizontalList } from '@/components/HorizontalList';
