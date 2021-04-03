c37c566b3169c143ec6b9537a6d598ae6995cc5a
Christian David Ibarguen <christianibarguen@gmail.com>
2021-04-03 14:55:02 -0500

    Add optional icon image in Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,8 @@
 import { useTheme } from '@react-navigation/native';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, Image, TouchableOpacity } from 'react-native';
+
 import { TextStyles } from '@/theme';
 
 const styles = StyleSheet.create({
@@ -15,14 +16,26 @@ const styles = StyleSheet.create({
   },
 });
 
-export function Button({ style, textStyle, title, ...rest }) {
+export function Button({
+  style: buttonStyle,
+  textStyle,
+  icon,
+  title,
+  ...rest
+}) {
   const { colors } = useTheme();
 
+  const style = buttonStyle || [styles.button, { borderColor: colors.border }];
+
   return (
-    <TouchableOpacity
-      style={[styles.button, { borderColor: colors.border }, style]}
-      {...rest}
-    >
+    <TouchableOpacity style={style} {...rest}>
+      {icon && (
+        <Image
+          source={icon}
+          accessibilityIgnoresInvertColors
+          style={{ tintColor: colors.invertedBase }}
+        />
+      )}
       <Text style={[{ color: colors.text }, TextStyles.label, textStyle]}>
         {title}
       </Text>
@@ -34,9 +47,11 @@ Button.propTypes = {
   style: PropTypes.object,
   textStyle: PropTypes.object,
   title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
 };
 
 Button.defaultProps = {
   style: null,
   textStyle: null,
+  icon: null,
 };
