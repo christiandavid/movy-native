1a0a9038c6791ff3df1c3453678c338400fb0d10
Jonathan <jpalacio0612@gmail.com>
2021-04-04 23:49:26 -0500

    Add user interface for login screen (#15)

--- src/screens/Login/Login.styles.js
+++ src/screens/Login/Login.styles.js
@@ -3,16 +3,25 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    padding: 40,
     justifyContent: 'center',
-    paddingHorizontal: 40,
+  },
+  logo: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   formContainer: {
-    borderRadius: 5,
-    padding: 20,
-    width: '100%',
+    marginVertical: 20,
   },
   submitButton: {
     marginTop: 20,
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    borderRadius: 10,
+    padding: 10,
+    color: 'white',
   },
 });
