faeb4f80a0f827c1578597ce26e945871eb444c3
Christian David Ibarguen <christianibarguen@gmail.com>
2021-04-05 07:36:49 -0500

    Small improvements

--- src/screens/Home/MyList/index.js
+++ src/screens/Home/MyList/index.js
@@ -25,8 +25,10 @@ export function MyList() {
   };
 
   useEffect(() => {
-    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
-  });
+    if (userList.length) {
+      LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+    }
+  }, [userList]);
 
   return (
     !!userList.length && (
